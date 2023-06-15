1115cbe67a0032db3c03b2af04ae94aebc5b9ce1
Jihnoy <tommystixxay@gmail.com>
2023-06-15 17:48:15 +0900

    新增nacos配置，orderserver增加RestTemplate

--- shop-order-server/src/main/java/product/service/impl/OrderServiceImpl.java
+++ shop-order-server/src/main/java/product/service/impl/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package product.service.impl;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import product.Product;
 import product.dao.OrderDao;
 import product.domain.Order;
@@ -15,10 +18,16 @@ import product.service.OrderService;
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderDao orderDao;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @Autowired
+    private RestTemplate restTemplate;
     @Override
     public Order createOrder(Long productId, Long userId){
         log.info("接收到{}号商品的下单请求,接下来调⽤商品微服务查询此商品信息", productId);
-        Product product = null;
+        ServiceInstance instance = discoveryClient.getInstances("product-service").get(0);
+        String url = instance.getHost()+":"+instance.getPort();
+        Product product = restTemplate.getForObject("http://"+url+"/product/"+productId, Product.class);
         log.info("查询到{}号商品的信息,内容是:{}", productId,
                 JSON.toJSONString(product));
         Order order = new Order();
